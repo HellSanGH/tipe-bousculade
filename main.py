c1fbb187a0a6e4b179a60d6dcd31d18dd4f77831
sewrslvtt <noormarfak@gmail.com>
2024-02-15 18:56:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 import pygame
+
+pygame.init()
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("TIPE : évolution")
+
+run = True
+while run: 
+    screen.fill((40, 35, 40))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            run = False
+
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
